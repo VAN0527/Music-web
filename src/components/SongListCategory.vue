ec1d57e2c9bea04aba663bc10e591b58a751b8dd
van <vanm52@gmail.com>
2019-04-27 22:27:06 +0800

    将歌单分类抽出, 简化 Songlist_vue js

---
+++ src/components/SongListCategory.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="category">
+    <div class="currentCat">
+      <span @click="toggleCatlist">
+        {{currentCat}} <i :class="[show ? 'icon-shrinkage' : 'icon-arrow-down']"></i>
+      </span>
+    </div>
+    <div class="catlist" v-show="show">
+      <div class="categories">
+        <div class="sublist">
+          <span 
+            class="sublist-item" 
+            :class="currentCat === '全部' ? 'active' : ''"
+            @click="selectItem('全部')"
+          >全部
+          </span>
+        </div>
+      </div>
+      <div
+        class="categories" 
+        v-for="cat in catlist"
+        :key="cat.name"
+      >
+        <div class="title">{{cat.name}}</div>
+        <div class="sublist">
+          <span 
+            class="sublist-item" 
+            :class="[currentCat === sub.name ? 'active' : '']" 
+            v-for="sub in cat.sub"
+            :key="sub.name"
+            @click="selectItem(sub.name)"
+          >
+            {{sub.name}}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getCatlist } from 'api/songlist.js'
+
+export default {
+  data () {
+    return {
+      show: false,
+      catlist: [],
+      currentCat: '全部',
+    }
+  },
+  created () {
+    this.$_getCatlist()
+  },
+  methods: {
+    toggleCatlist () {
+      this.show = !this.show
+    },
+    selectItem (cat) {
+      if (this.currentCat === cat) {
+        this.show = false
+        return
+      }
+
+      this.currentCat = cat
+      this.show = false
+      this.$emit('select', cat)
+    },
+    $_getCatlist () {
+      getCatlist().then(res => {
+        if (res.status === 200) {
+          this.catlist = this.$_normalizeCatlist(res.data)
+        }
+      })
+    },
+    $_normalizeCatlist (catlist) {
+      const categories = catlist.categories
+      const subs = catlist.sub
+      let result = []
+
+      for (let item in categories) {
+        result.push({
+          name: categories[item],
+          category: item,
+          sub: []
+        })
+      }
+
+      subs.forEach(sub => {
+        result.forEach(item => {
+          if (sub.category == item.category) {
+            item.sub.push(sub)
+          }
+        })
+      })
+
+      return result
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'styles/variable.scss';
+.category {
+  position: relative;
+
+  .currentCat {
+    margin: 10px;
+    font-size: 1.5em;
+    
+    span {
+      cursor: pointer;
+    }
+  }
+  
+  .catlist {
+    position: absolute;
+    width: 100%;
+    background-color: rgb(235, 235, 235);
+    z-index: 1;
+
+    .categories {
+      padding: 20px 10px;
+      border-bottom: 1px solid rgb(0, 0, 0);
+      
+      &:last-child {
+        border-bottom: none;
+      }
+
+      .title {
+        margin-bottom: 10px;
+        font-size: 1.2em;
+      }
+
+      .sublist {
+        display: flex;
+        flex-wrap: wrap;
+
+        .sublist-item {
+          margin: 5px 10px;
+          padding: 5px 10px;
+          background-color: rgb(252, 251, 251);
+          border-radius: 10px;
+          cursor: pointer;
+
+          &.active {
+            background-color: $text-hover-color;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/SongList.vue
+++ src/views/SongList.vue
@@ -1,41 +1,7 @@
 <template>
   <div class="songlist">
     <div class="catlist-wrapper">
-      <div class="currentCat">
-        <span @click="toggleCatlist">
-          {{currentCat}} <i :class="[show ? 'icon-shrinkage' : 'icon-arrow-down']"></i>
-        </span>
-      </div>
-      <div class="catlist" v-show="show">
-        <div class="categories">
-          <div class="sublist">
-            <span 
-              class="sublist-item" 
-              :class="currentCat === '全部' ? 'active' : ''"
-              @click="selectItem('全部')"
-            >全部
-            </span>
-          </div>
-        </div>
-        <div
-          class="categories" 
-          v-for="cat in catlist"
-          :key="cat.name"
-        >
-          <div class="title">{{cat.name}}</div>
-          <div class="sublist">
-            <span 
-              class="sublist-item" 
-              :class="[currentCat === sub.name ? 'active' : '']" 
-              v-for="sub in cat.sub"
-              :key="sub.name"
-              @click="selectItem(sub.name)"
-            >
-              {{sub.name}}
-            </span>
-          </div>
-        </div>
-      </div>
+      <SongListCategory @select="selectItem"></SongListCategory>
     </div>
     <div class="songlist-wrapper">
       <List 
@@ -50,151 +16,57 @@
 <script>
 import List from 'components/List'
 import Loading from 'components/Loading'
-import { getCatlist, getSonglist } from 'api/songlist.js'
-import { mapMutations } from 'vuex'
+import SongListCategory from 'components/SongListCategory'
+import { getSonglist } from 'api/songlist.js'
 
 export default {
   components: {
     List,
-    Loading
+    Loading,
+    SongListCategory
   },
   data () {
     return {
-      show: false,
-      catlist: [],
-      currentCat: '全部',
       songlist: [],
       loading: true
     }
   },
   created () {
-    this.$_getCatlist()
     this.$_getSonglist()
   },
   methods: {
-    toggleCatlist () {
-      this.show = !this.show
-    },
     selectItem (cat) {
-      this.currentCat = cat
-      this.show = false
-      this.$_getSonglist()
+      this.$_getSonglist(cat)
     },
     selectSongList (songlist) {
       this.$router.push({
         path: `/musiclist/${songlist.id}`
       })
-      this.setMusicList(songlist)
     },
-    $_getCatlist () {
-      getCatlist().then(res => {
-        if (res.status === 200) {
-          this.catlist = this.$_normalizeCatlist(res.data)
-        }
-      })
-    },
-    $_getSonglist () {
+    $_getSonglist (cat = '全部') {
       this.loading = true
       this.songlist = []
-      getSonglist(this.currentCat).then(res => {
-        if (res.status === 200) {
-          let data = res.data.playlists.map(item => {
-            return {
-              id: item.id,
-              name: item.name,
-              picUrl: `${item.coverImgUrl}?param=400y400`
-            }
-          })
-          this.songlist = data
-          this.loading = false
+
+      getSonglist(cat).then(res => {
+        this.songlist = this.$_formatList(res.data.playlists)
+        this.loading = false
+      })
+    },
+    $_formatList (list) {
+      return list.map(item => {
+        return {
+          id: item.id,
+          name: item.name,
+          picUrl: `${item.coverImgUrl}?param=400y400`
         }
       })
-    },
-    $_normalizeCatlist (catlist) {
-      let ret = []
-      let categories = catlist.categories
-      let subs = catlist.sub
-
-      for (let item in categories) {
-        ret.push({
-          name: categories[item],
-          category: item,
-          sub: []
-        })
-      }
-
-      subs.forEach(sub => {
-        ret.forEach(item => {
-          if (sub.category == item.category) {
-            item.sub.push(sub)
-          }
-        })
-      })
-
-      return ret
-    },
-    ...mapMutations({
-      setMusicList: 'SET_MUSIC_LIST'
-    })
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import 'styles/variable.scss';
-
 .songlist {
-  .catlist-wrapper {
-    position: relative;
-
-    .currentCat {
-      margin: 10px;
-      font-size: 1.5em;
-      
-      span {
-        cursor: pointer;
-      }
-    }
-    
-    .catlist {
-      position: absolute;
-      width: 100%;
-      background-color: rgb(235, 235, 235);
-      z-index: 1;
-
-      .categories {
-        padding: 20px 10px;
-        border-bottom: 1px solid rgb(0, 0, 0);
-        
-        &:last-child {
-          border-bottom: none;
-        }
-
-        .title {
-          margin-bottom: 10px;
-          font-size: 1.2em;
-        }
-
-        .sublist {
-          display: flex;
-          flex-wrap: wrap;
-
-          .sublist-item {
-            margin: 5px 10px;
-            padding: 5px 10px;
-            background-color: rgb(252, 251, 251);
-            border-radius: 10px;
-            cursor: pointer;
-
-            &.active {
-              background-color: $text-hover-color;
-            }
-          }
-        }
-      }
-    }
-  }
-
   .songlist-wrapper {
     position: relative;
   }
